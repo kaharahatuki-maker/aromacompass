49eeec9a07e03b25ed152fad0a2a9328e81207b9
kaharahatuki-maker <kahara.hatuki@gmail.com>
2026-04-27 17:30:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+console.log("✅ script.js は読み込まれました");
+
 document.getElementById("morning").addEventListener("click", function () {
+  console.log("✅ 朝ボタンがクリックされました");
   location.href = "aroma-diagnosis-q2.html";
 });

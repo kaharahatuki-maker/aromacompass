734f9d370bc5815c7aecb282f674e1ac3bd84d94
kaharahatuki-maker <kahara.hatuki@gmail.com>
2026-05-11 14:23:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ document.getElementById("morning").addEventListener("click", function () {
 });
 
 const resultArea = document.getElementById("pick-up-result");
+const buttons = document.querySelectorAll("button")
+const API_URL = "https://script.google.com/macros/s/AKfycby2iNLCwQxlcjG2j98dx6M2p4u31c7MXPsSAsMFgVn1hXrcIDuNrefNl3v6gOTauWYJ/exec";
+
+buttons.forEach(button) => {
+  button.addEventListener("click",async () => {
+    const questionId = Number(button.dataset.question);
+
+    try{
+
+      const response = await fetch(APL_URL);
+      const data = await response.json();
+
+      const filtered = data.filter(
+        (item) => item.question_id === questionId
+      );
+
+      const randomItem =
+        filtered[math.floor(math.random() * filtered.length)];
+    }
+  }
+}

eb07e30ff44d8674cec22147d4807a67b5ee3d30
kaharahatuki-maker <kahara.hatuki@gmail.com>
2026-05-11 14:11:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementById("morning").addEventListener("click", function () {
   location.href = "aroma-diagnosis-q2.html";
 });
+
+const resultArea = document.getElementById("pick-up-result");

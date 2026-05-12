057b14ed3454b188f1b63b8b48b5538677e2f91a
kaharahatuki-maker <kahara.hatuki@gmail.com>
2026-05-12 14:42:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ document.querySelector("#morning").addEventListener("click", function () {
 const picIp_API_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnQEbFU-pIKCbmVlADM0YotnkooiZ4dUOehPJGcI2OKFA2mdembQgHHU4Lkz4U-m2B3nRWZgHm5L2B_J_CX9nHgEHt-UUPAC2jA1EfEkV86aI9VLUtnDAS4B9-otLXsNlD0fXAGlykL4EDSTvTDjoU6gFhAFKG0-AOcUNnBH3kzK7lTCf8dNdUGUjt_FnSum6qqk5OVEHqVnH7bh-iiNpKYAgzzR97QB9fL3SqhqQ_0ucskTthizggi6Ni7atQ6Qg1btxbp7OCoV_V5vuKX6q9PfkbtBiA&lib=MnHJmmCJIrb3izUs2fkndKSkAf2tFMscx"
 
 //【質問ページ】
-const questionbuttons = document.querySelectorAll("{[data-question]");
+const questionbuttons = document.querySelectorAll("[data-question]");
 if(questionbuttons.length > 0){
-  questionbuttons.forEach("click",function(){
+  questionbuttons.forEach(function(button){
+    button.addEventListener("click",function(){
     const questionId = this.dataset.question;
 
     //選択した番号を保存
@@ -22,12 +23,12 @@ if(questionbuttons.length > 0){
 }
 
 //【結果ページ】
-const pickUpResult = document.querySelector(#pickUpResult);
+const pickUpResult = document.querySelector("#pickUpResult");
 if(pickUpResult){
   async function loadResult(){
     const questionId = localStorage.getItem("questionId");
     const response = await fetch(picIp_API_URL);
-    const data = await resoinse.json();
+    const data = await response.json();
 
     //questionIdが一致するデータだけ取得
     const results = data.filter(item =>

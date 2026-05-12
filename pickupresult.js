fe0f66b1931840d9ece46264ee19f1814ca4ba3b
kaharahatuki-maker <kahara.hatuki@gmail.com>
2026-05-12 15:17:47 +0900

    Update pickupresult.js

--- pickupresult.js
+++ pickupresult.js
@@ -1,6 +1,6 @@
 //■今日のピックアップ結果表示
 //本日のピックアップデータ取得用URL
-const picIp_API_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnQEbFU-pIKCbmVlADM0YotnkooiZ4dUOehPJGcI2OKFA2mdembQgHHU4Lkz4U-m2B3nRWZgHm5L2B_J_CX9nHgEHt-UUPAC2jA1EfEkV86aI9VLUtnDAS4B9-otLXsNlD0fXAGlykL4EDSTvTDjoU6gFhAFKG0-AOcUNnBH3kzK7lTCf8dNdUGUjt_FnSum6qqk5OVEHqVnH7bh-iiNpKYAgzzR97QB9fL3SqhqQ_0ucskTthizggi6Ni7atQ6Qg1btxbp7OCoV_V5vuKX6q9PfkbtBiA&lib=MnHJmmCJIrb3izUs2fkndKSkAf2tFMscx"
+const pickUp_API_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnQEbFU-pIKCbmVlADM0YotnkooiZ4dUOehPJGcI2OKFA2mdembQgHHU4Lkz4U-m2B3nRWZgHm5L2B_J_CX9nHgEHt-UUPAC2jA1EfEkV86aI9VLUtnDAS4B9-otLXsNlD0fXAGlykL4EDSTvTDjoU6gFhAFKG0-AOcUNnBH3kzK7lTCf8dNdUGUjt_FnSum6qqk5OVEHqVnH7bh-iiNpKYAgzzR97QB9fL3SqhqQ_0ucskTthizggi6Ni7atQ6Qg1btxbp7OCoV_V5vuKX6q9PfkbtBiA&lib=MnHJmmCJIrb3izUs2fkndKSkAf2tFMscx"
 
 //【質問ページ】
 const questionbuttons = document.querySelectorAll("[data-question]");
@@ -23,7 +23,7 @@ const pickUpResult = document.querySelector("#pickUpResult");
 if(pickUpResult){
   async function loadResult(){
     const questionId = localStorage.getItem("questionId");
-    const response = await fetch(picIp_API_URL);
+    const response = await fetch(pickUp_API_URL);
     const data = await response.json();
 
     //questionIdが一致するデータだけ取得

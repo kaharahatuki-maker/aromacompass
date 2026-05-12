fdb5aa3e3ada04ee19646ceac651ccd8a343ff3d
kaharahatuki-maker <kahara.hatuki@gmail.com>
2026-05-12 14:44:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ if(pickUpResult){
     //questionIdが一致するデータだけ取得
     const results = data.filter(item =>
       item.question_id == questionId
-                               );
+  );
     //ランダムで1つ選ぶ
     const randomResult = 
       results[Math.floor(Math.random() * results.length)];

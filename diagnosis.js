5aba1f6247b2e00cbb8197c052b8ddcd0afd717e
kaharaHatuki <kahara.hatuki@gmail.com>
2026-06-19 13:39:11 +0900

    Update diagnosis.js

--- diagnosis.js
+++ diagnosis.js
@@ -1,1 +1,38 @@
+//==========
+//Q1.配慮事項
+//==========
+const cautionButtons = document.querySelectorAll("[data-caution]");
+if(cautionButtons.length > 0) {
+  cautionButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      localStorage.setItem(
+        "caution",
+        button.dataset.caution
+      );
+    });
+  });
+}
 
+//==========
+//Q2.使用時間
+//==========
+
+//==========
+//Q3.目的
+//==========
+
+//==========
+//Q4.香りの系統
+//==========
+
+//==========
+//Q5.重さ
+//==========
+
+//==========
+//Q6.甘さ
+//==========
+
+//==========
+//診断結果
+//==========
